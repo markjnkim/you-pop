7088c47762b97171e0179aedf8c1a14458d0e25c
trumpocalyspe <mark.jn.kim@gmail.com>
2018-11-27 16:05:21 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+/* Load the HTTP library */
+var http = require("http");
+var fs = require('fs');
+
+/* Create an HTTP server to handle responses */
+fs.readFile('./index.html', function (err, html) {
+  if(err) {
+    throw err;
+  }
+
+  var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(html);
+    response.end();
+  }).listen(8888, '127.0.0.1', () => {
+    // Console
+    console.log(`Server running at http://localhost:8888`);
+  });
+});
