8d215639588accb58ca58dd2bfb72150909005ff
trumpocalyspe <mark.jn.kim@gmail.com>
2018-11-30 13:04:05 -0800

    process bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const get_api = function(talent) {
   const BASE_URL = 'https://api.spotify.com/v1/search?';
   const FETCH_URL = BASE_URL + 'q=' + talent + '&type=artist&limit=1';
-  var accessToken = process.env.A_TOKEN;
+  const accessToken = process.env.A_TOKEN;
   
   // 'BQBKd7G9JCFlSW55EPMdwF1NNr4ja1UTB-Z04GoskVC06bQMkhfrD_bsMoYgNUnAQ7sFOaPhBRDUpS59MwunKxCRyz1xW1HoTtHV82wmgXY3LwvF7efNp0qyChiWcYurSGLytslM6flghD7RxfArNd9hv-BBk-d4&refresh_token=AQBBDcUL8P_abNn2FQxU1TGLrekhZ6jfkZ6HyC2x8ps3uiQW1DX61djJv7IK8lTd244R7q4IZmF4u4m7U5DpMz4NmGHZ9f-j7mPWYj--XImzAaQKOWBoSNpabTw4-6YezHqs9g';
 
