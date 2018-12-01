777ccd1422021e824791be91479b7b9142640d13
trumpocalyspe <mark.jn.kim@gmail.com>
2018-12-01 10:29:52 -0800

    styling active img, receiving the id from clicked event

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ const getApi = function() {
   const FETCH_URL = BASE_URL + 'q=year:' + year + '&type=artist&market=US&limit=1';
   // "https://api.spotify.com/v1/search?q=year%3A2001&type=artist&market=US&limit=1&offset=12345&popularity:8-100"
   console.log(year);
-  const accessToken = 'BQDBsdoZ1HguqN-j3euAqXqT-16hpPp0GkGDOT5J1ROn97zIgrCe5RlUsh01Fom8PsxIlRqwZLAiXMeOdN_b2JM7iSrUtsaFWO9a3AqjOyiNShXZlJw3QS09tPwn87kOb7jAhGeU_DM0mi07EiNULS3Ev9XveHLG&refresh_token=AQD10NtmTm93j0RiXejhSGV1kact1CaJ-VUCI2-8OZR_B-0Vizt_xi1CIXcfprJgCfeo4NVi1kjKlDJmGL2UFWku28dBduOL54LF8h8qZwtqJ-qXNG7u52YTdw-MxYk8noULvw';
+  const accessToken = 'BQBMJzIvq8bA8aMYt5DlERHskre_wBLXYF6fqIVZVvZaKzQqbExSrTjj1BEiJ048ABPwHdFTjjO8QSduDxDBfYrAvQ0Trtrpyg-DbkKlEvxUWUtRxzM-wPqrD3gUInWhIhDPVGqt_oQs8xSK86B6YaA0e-hKWNxU&refresh_token=AQA4X6-vG8vee-EMLrut6Hft-yrAkZD8lJ1t99qtSCYNJvb3zpWslr1RurvvbegbLExSUxhxCHFNjbRR7Pjw4Jzk0zm8JCNPPzRnE-QmkRv3LRwGjWKfhEzq_2ih9o_KDErvVg';
 
   var myOptions = {
     method: 'GET',
@@ -51,7 +51,7 @@ const getApi = function() {
       img.setAttribute("id","artist-2");
       img.setAttribute("value", artist.popularity);
     }
-    // img.setAttribute("onclick", `getPop(${this})`);
+    img.setAttribute("onclick", "getPop(this.id)");
     // const onePop=artist.popularity;
     // console.log(onePop);
 
@@ -67,8 +67,18 @@ const getApi = function() {
 };
 
 
-function getPop(id) {
-  console.log(id);
+function getPop(artist_id) {
+  console.log(artist_id);
+  // window.onload = function() {
+  //   const answer = document.getElementById("answer");answer.innerHTML("Winner");  
+  //   document.getElementById("artist-1").addEventListener( 'click', function() {
+  //     alert("Hello");
+  //   });
+
+  //   document.getElementById("artist-2").addEventListener( 'click', function() {
+  //     alert("GoodBye");
+  //   });
+  // };
 
   // document.getElementById("answer").innerHTML("Winner!");
 }
@@ -76,19 +86,16 @@ function getPop(id) {
 const artistOne = getApi();
 setTimeout( function() {
   getApi();
-}, 10 );
+}, 100 );
 
-setTimeout( function() {
-  window.onload = function(){
-    document.getElementById("artist-1").addEventListener( 'click', function() {
-      console.log('Hello');
-      // this.getAttribute("id")
-    });
-    document.getElementById("artist-2").addEventListener( 'click', function() {
-      console.log("GoodBye");
-      // this.getAttribute("value")
-    });
-  };
-}, 1000);
+// setTimeout( function() {
+//   window.onload = function() {
+//     document.getElementById("artist-1").addEventListener( 'click', function() {
+//       alert("Hello");
+//     });
 
-
+//     document.getElementById("artist-2").addEventListener( 'click', function() {
+//       alert("GoodBye");
+//     });
+//   };
+// }, 1000 );
