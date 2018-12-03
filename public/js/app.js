e63f6cad85d3dff53816a9899ce5fbf0265a1480
trumpocalyspe <mark.jn.kim@gmail.com>
2018-12-02 17:13:24 -0800

    added function for artist ID search

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,39 @@
 // require('dotenv').config();
 // const accessToken = process.env.A_TOKEN;
-let artOne, artTwo;
+// let artOne, artTwo;
+let artistArray = [];
+
+const getApiArtist = function() {
+  const BASE_URL = 'https://api.spotify.com/v1/search?';
+  const name = "aerosmith";
+  const FETCH_URL = BASE_URL + 'q=' + name + '&type=artist&market=US&limit=10';
+  // "https://api.spotify.com/v1/search?q=year%3A2001&type=artist&market=US&limit=1&offset=12345&popularity:8-100"
+  // console.log(year);
+  const accessToken = 'BQC4BlkaLifg42Oi0VLVMsAW_buUHtDofb5nQbjl-JlUU_5wf-AqFm6tsK6SKTkOTqtRvVuxLzyuNfpfBwTb7ZpNhI_uZUUNh8K32mcBhqc3soZ3lDERiLGtPBtpKZ9yR2PXeuuMayqpV0gVcgFSfAa326at7Y4B&refresh_token=AQBPDDVqO440PZW5U2cNVFu6PvQrsdEc3ODpdg5dmzf-thQ0hZgxcavnEUFlwxRllOENqv1TqFswrxSQeBL0MfFYrM9I2GRK09Z5BaHAGZuSjk4Qg-bZ2Dfl2GuHL4LJfvnz8g';
+
+  var myOptions = {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    },
+    mode: 'cors',
+    cache: 'default'
+  };
+  
+
+    fetch(FETCH_URL, myOptions)
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];        
+      console.log(json.artists);
+      // render_data(artist);
+      return artist;
+    });
+  
+    
+};
+
+
 const getApi = function() {
   const BASE_URL = 'https://api.spotify.com/v1/search?';
   // let artOne, artTwo;
@@ -10,7 +43,7 @@ const getApi = function() {
   const FETCH_URL = BASE_URL + 'q=year:' + year + '&type=artist&market=US&limit=1';
   // "https://api.spotify.com/v1/search?q=year%3A2001&type=artist&market=US&limit=1&offset=12345&popularity:8-100"
   console.log(year);
-  const accessToken = 'BQB9tHHo8BW_aP-32JSIaj6msgEjUh1WwXicKX_76rD5SNpaMWXGcr-jtixWMYkiuj9w3IrbSdZC2Gar2U7jOTopr32wj_D7nH9Zg1yOEXZqOeD5N4vKY591PyzdCJIzW0iSmry6ULUnm76vFbeU7SQcVdrPHbqB&refresh_token=AQBkxyNEvOU7qU7Y_8gKiAzK53PDCpcAXJIBFpvzWiNZbvK9NNZP47EsVAkeoaGq6yWqm2Qoz8Bt1UHKHJwBIdPZCnvlKqp3CUL4bI0vIbmR8ojTjx3h05bY3hD6HlWA7MGPBw';
+  const accessToken = 'BQC4BlkaLifg42Oi0VLVMsAW_buUHtDofb5nQbjl-JlUU_5wf-AqFm6tsK6SKTkOTqtRvVuxLzyuNfpfBwTb7ZpNhI_uZUUNh8K32mcBhqc3soZ3lDERiLGtPBtpKZ9yR2PXeuuMayqpV0gVcgFSfAa326at7Y4B&refresh_token=AQBPDDVqO440PZW5U2cNVFu6PvQrsdEc3ODpdg5dmzf-thQ0hZgxcavnEUFlwxRllOENqv1TqFswrxSQeBL0MfFYrM9I2GRK09Z5BaHAGZuSjk4Qg-bZ2Dfl2GuHL4LJfvnz8g';
 
   var myOptions = {
     method: 'GET',
@@ -32,7 +65,45 @@ const getApi = function() {
     });
   
     
+};
+
+const getSeveralApi = function() {
+  const BASE_URL = 'https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/related-artists';
+  // let artOne, artTwo;
+  //randomly selects from 1962 - 2018  year = [1902-2019)
+  const year = Math.floor(Math.random()*(2019-1902+1)+1962);
+  // const yr = 2001;
+  const FETCH_URL = BASE_URL + 'q=year:' + year + '&type=artist&market=US&limit=1';
+  // "https://api.spotify.com/v1/search?q=year%3A2001&type=artist&market=US&limit=1&offset=12345&popularity:8-100"
+  console.log(year);
+  const accessToken = 'BQC4BlkaLifg42Oi0VLVMsAW_buUHtDofb5nQbjl-JlUU_5wf-AqFm6tsK6SKTkOTqtRvVuxLzyuNfpfBwTb7ZpNhI_uZUUNh8K32mcBhqc3soZ3lDERiLGtPBtpKZ9yR2PXeuuMayqpV0gVcgFSfAa326at7Y4B&refresh_token=AQBPDDVqO440PZW5U2cNVFu6PvQrsdEc3ODpdg5dmzf-thQ0hZgxcavnEUFlwxRllOENqv1TqFswrxSQeBL0MfFYrM9I2GRK09Z5BaHAGZuSjk4Qg-bZ2Dfl2GuHL4LJfvnz8g';
+
+  var myOptions = {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    },
+    mode: 'cors',
+    cache: 'default'
   };
+  
+  // Loop to capture 2 artists
+    fetch(BASE_URL, myOptions)
+    .then(response => response.json())
+    .then(json => {
+      artistArray = json.artists;        
+      renderTwo(artistArray);
+      // return artists;
+      console.log(artistArray);
+    });
+};
+
+function renderTwo(array) {
+  for(let i = 0; i < array.length; i++){
+    render_data(array.shift());  
+  }
+}
+
 function render_data(artist) {
   
   const artist_container = document.getElementById("artist");
@@ -67,28 +138,34 @@ function render_data(artist) {
   // div.appendChild(hr);
 };
 
-
 function getPop(artist) {
   // If you choose the first image/artist
   if (artist.id === "artist-1"){
     const artOne = artist;
-    artTwoPop = document.getElementById("artist-2").getAttribute("name");
+    const artTwoPop = document.getElementById("artist-2").getAttribute("name");
     console.log(`artTwoPop: ${artTwoPop}`);
     console.log(`artOnePop: ${artOne.name}`);
     const answer = document.getElementById("answer");
+    // answer.innerHTML= "Winner Winner Chicken Dinner!";
+    // const answerDiv = document.createElement('div');
     if( parseInt(artOne.name) > parseInt(artTwoPop) ) {
-      const answerDiv = document.createElement('div');
-      answerDiv.innerHTML("Winner Winner Chicken Dinner!");
-      answer.append(answerDiv);
+      console.log(typeof artOne.name);
+      answer.innerHTML = "Winner Winner Chicken Dinner!";
     }else{
-      document.getElementById("answer").textContent("Loser Loser We Got a Snoozer!");
+      answer.innerHTML = "Loser Loser We Got a Snoozer!";
     }
+    // answer.append(answerDiv);
+
+    // You picked artist 2
   }else { 
-    const artTwo = pop;
-    let artOne = {};
-    artOne.id = document.getElementById("artist-2").getAttribute("id");
-    artOne.name = document.getElementById("artist-2").getAttribute("name");
-    console.log(artOne);
+    const artTwo = artist;
+    const artOnePop = document.getElementById("artist-1").getAttribute("name");;
+    if( parseInt(artTwo.name) > parseInt(artOnePop) ) {
+      // console.log(typeof artOne.name);
+      answer.innerHTML = "Winner Winner Chicken Dinner!";
+    }else{
+      answer.innerHTML = "Loser Loser We Got a Snoozer!";
+    }
   }
   
 
@@ -113,22 +190,22 @@ function getPop(artist) {
   // document.getElementById("answer").innerHTML("Winner!");
 }
 
-const artistOne = getApi();
-console.log(artistOne);
-
-const artistTwo = setTimeout( function() {
-  getApi();
-}, 100 );
-console.log(artistTwo);
-
+//Several artists.
+// getSeveralApi();
+// getApi();
 // setTimeout( function() {
-//   window.onload = function() {
-//     document.getElementById("artist-1").addEventListener( 'click', function() {
-//       alert("Hello");
-//     });
+//   getApi();
+// }, 9 );
+getApiArtist();
+setTimeout( function() {
+  getApiArtist();
+}, 90 );
 
-//     document.getElementById("artist-2").addEventListener( 'click', function() {
-//       alert("GoodBye");
-//     });
-//   };
-// }, 1000 );
+
+document.onkeyup = function(event) {
+  const key = event.key;
+  document.getElementById("artist").innerHTML = "";
+  document.getElementById("answer").innerHTML = "";
+  renderTwo(artistArray);
+  console.log(key);
+};
---
+++ public/js/artist.js
@@ -0,0 +1,6 @@
+export const artists_id = {
+  "Madonna": "6tbjWDEIzxoDsBA1FuhfPW",
+  "Prince": "5a2EaR3hamoenG9rDuVn8j",
+  "Elvis Presley": "43ZHCT0cAZBISjO8DG9PnE",
+  "The Beatles": "3WrFJ7ztbogyGnTHbHJFl2"
+};
